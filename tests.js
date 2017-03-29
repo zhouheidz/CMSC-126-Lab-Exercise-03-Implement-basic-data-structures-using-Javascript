40cf5dc740c1f7b6ab85fcd3d5888df0cf0606a7
Arnelle Balane <arnellebalane@gmail.com>
2017-03-29 11:26:28 +0800

    test cases for stack data structur:

--- tests.js
+++ tests.js
@@ -118,3 +118,127 @@ describe('LinkedList', function() {
         });
     });
 });
+
+
+describe('Stack', function() {
+    describe('push(value)', function() {
+        beforeEach(function() {
+            this.stack = new Stack();
+        });
+
+        it('should add the value to the end of the stack', function() {
+            this.stack.push(1);
+            assert.deepEqual(this.stack.items(), [1]);
+
+            this.stack.push(2);
+            assert.deepEqual(this.stack.items(), [1, 2]);
+
+            this.stack.push(3);
+            assert.deepEqual(this.stack.items(), [1, 2, 3]);
+        });
+
+        it('should increment the length of the stack', function() {
+            this.stack.push(1);
+            assert.equal(this.stack.length, 1);
+
+            this.stack.push(2);
+            assert.equal(this.stack.length, 2);
+
+            this.stack.push(3);
+            assert.equal(this.stack.length, 3);
+        });
+    });
+
+    describe('pop()', function() {
+        beforeEach(function() {
+            this.stack = new Stack();
+            for (var i = 1; i <= 3; i++) {
+                this.stack.push(i);
+            }
+        });
+
+        it('should remove the value at the top of the stack', function() {
+            this.stack.pop();
+            assert.deepEqual(this.stack.items(), [1, 2]);
+
+            this.stack.pop();
+            assert.deepEqual(this.stack.items(), [1]);
+
+            this.stack.pop();
+            assert.deepEqual(this.stack.items(), []);
+
+            this.stack.pop();
+            assert.deepEqual(this.stack.items(), []);
+        });
+
+        it('should return the removed value, or null if none', function() {
+            assert.equal(this.stack.pop(), 3);
+            assert.equal(this.stack.pop(), 2);
+            assert.equal(this.stack.pop(), 1);
+            assert.equal(this.stack.pop(), null);
+        });
+
+        it('should decrement the length of the stack', function() {
+            this.stack.pop();
+            assert.equal(this.stack.length, 2);
+
+            this.stack.pop();
+            assert.equal(this.stack.length, 1);
+
+            this.stack.pop();
+            assert.equal(this.stack.length, 0);
+        });
+    });
+
+    describe('peek()', function() {
+        beforeEach(function() {
+            this.stack = new Stack();
+            for (var i = 1; i <= 3; i++) {
+                this.stack.push(i);
+            }
+        });
+
+        it('should return the value at the top of the stack', function() {
+            assert.equal(this.stack.peek(), 3);
+            assert.equal(this.stack.peek(), 3);
+            assert.equal(this.stack.peek(), 3);
+        });
+
+        it('should not remove any value from the stack', function() {
+            this.stack.peek();
+            assert.deepEqual(this.stack.items(), [1, 2, 3]);
+
+            this.stack.peek();
+            assert.deepEqual(this.stack.items(), [1, 2, 3]);
+
+            this.stack.peek();
+            assert.deepEqual(this.stack.items(), [1, 2, 3]);
+        });
+
+        it('should not modify the length of the stack', function() {
+            this.stack.peek();
+            assert.equal(this.stack.length, 3);
+
+            this.stack.peek();
+            assert.equal(this.stack.length, 3);
+
+            this.stack.peek();
+            assert.equal(this.stack.length, 3);
+        });
+    });
+
+    describe('items()', function() {
+        beforeEach(function() {
+            this.stack = new Stack();
+            for (var i = 1; i <= 5; i++) {
+                this.stack.push(i);
+            }
+        });
+
+        it('should return an array of values in the stack', function() {
+            var items = this.stack.items();
+            assert(items instanceof Array);
+            assert.deepEqual(items, [1, 2, 3, 4, 5]);
+        });
+    });
+});
